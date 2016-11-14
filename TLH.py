bf7d20018ea8b97abb62b106c5a2293b2b6ef394
ilvitorio <vdc_hernandez@hotmail.com>
2016-11-14 17:39:14 -0300

    TLH Initial PY

--- TLH.py
+++ TLH.py
@@ -1,3 +1,21 @@
 """ Tax Algo """
 
 import pandas as pd
+
+
+def get_last_prices():
+    "Gets the data from the system"
+    "returns: A pandas dataframe with the financial data"
+    data_day = pd.read_csv("C:\\Users\\U447354\\Documents\\Python Scripts\\Beta\\Last_Prices.csv",index_col=0,header=0)
+    
+    return(data_day)
+
+def get_portfolio():
+    "Gets the clients portfolio Holding Data"
+    "returns: The clients portfolio data"
+    holdings = pd.read_csv("C:\\Users\\U447354\\Documents\\Python Scripts\\Beta\\Portfolio.csv",index_col=0,header=0)
+    
+    return(holdings)
+    
+def get_trade_history():
+    "Gets all the trade history for the client"
